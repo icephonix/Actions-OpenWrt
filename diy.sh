fcff9f732c78da5da8a35550c5acf592e46d75fa
icephonix <43659237+icephonix@users.noreply.github.com>
2020-01-31 12:59:35 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,3 +9,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 git clone https://github.com/milaq/openwrt_knockd.git package/knockd
+git clone https://github.com/KFERMercer/openwrt-adguardhome.git package/adguardhome
